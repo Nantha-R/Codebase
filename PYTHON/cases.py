adb978f6c81ec6fd53fca5d1a15969aa8e93d2d2
Nantha-R <nknanthakumarcr7@gmail.com>
2018-06-16 17:51:14 +0530

    CASES and PRINT implementation in PYTHON

---
+++ PYTHON/cases.py
@@ -0,0 +1,9 @@
+# Dealing with cases
+
+str = "Codebase"
+if str[0].isupper(): # Returns true as C is upper case
+    pass
+a = str[1].upper()
+print(a) # Value of a is equal to 'O'
+
+# Similarly islower() and lower() built-in functions are present in python
---
+++ PYTHON/print.py
@@ -0,0 +1,6 @@
+# Dealing with print statements
+
+print("abc",end='') # To print in python without new line feed
+
+f = 0.12345
+print("{:.2f}".format(f)) # To print upto two decimals
